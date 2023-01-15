9e32c9b4317603e2b2b5de797de790dda6d4c824
Samueelx <gsammygitau@gmail.com>
2023-01-15 23:15:45 +0300

    Done with 2 out of 3 Deliverables.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [cart, setCart] = useState("");
+  const [textContent, setContent] = useState("Add to Cart");
+
+  function handleOnClick() {
+    setCart("in-cart");
+    setContent("Remove From Cart");
+  }
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleOnClick}>{textContent}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setCategory] = useState("All");
+  function handleChange(e) {
+    setCategory(() => e.target.value);
+    items = items.filter(item => item.category === selectedCategory);
+  }
+  console.log(selectedCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={ handleChange }>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
