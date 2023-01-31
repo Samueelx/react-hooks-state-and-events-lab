f4e0ca4e0d2f390150149a12bbb3e974fcabc4a6
Samueelx <gsammygitau@gmail.com>
2023-01-31 15:10:07 +0300

    completed assignment.

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,22 +5,31 @@ function ShoppingList({ items }) {
   const [selectedCategory, setCategory] = useState("All");
   function handleChange(e) {
     setCategory(() => e.target.value);
-    items = items.filter(item => item.category === selectedCategory);
   }
-  console.log(selectedCategory);
+
+  const updatedList = items.filter(list => {
+    if(selectedCategory ==="All"){
+      return true
+    }else{
+    return list.category === selectedCategory
+    }
+  }) 
+  // if(selectedCategory !== "All") {
+  //   items = items.filter(item => item.category === selectedCategory);
+  // }
 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" value={selectedCategory} onChange={ handleChange }>
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={ handleChange }>
+          <option value="All">{selectedCategory}</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {updatedList.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
